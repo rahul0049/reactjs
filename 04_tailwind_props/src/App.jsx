87f6cdeab4bcb58aa731dfabcfa2dc1c67d4ab2d
Rahul <rahulrajoriya089@gmail.com>
2025-12-24 17:44:16 +0530

    tailwind config

---
+++ 04_tailwind_props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+ 
+  return (
+    <>
+      <h1 className='bg-green-400 text-blue-50 p-4 rounded-xl'>Tailwind Test</h1>
+      
+    </>
+  )
+}
+
+export default App
